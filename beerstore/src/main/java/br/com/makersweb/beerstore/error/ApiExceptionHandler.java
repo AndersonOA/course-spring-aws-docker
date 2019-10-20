68fcdb3cfb1b3e7c1dd66da007f5c1ffd807ec64
Anderson O. Aristides <anderson.aristides@fh.com.br>
2019-10-19 21:38:45 -0300

    Feat: Ajuste no tratamento de Excptions Handlers

--- beerstore/src/main/java/br/com/makersweb/beerstore/error/ApiExceptionHandler.java
+++ beerstore/src/main/java/br/com/makersweb/beerstore/error/ApiExceptionHandler.java
@@ -6,6 +6,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.MessageSource;
 import org.springframework.context.NoSuchMessageException;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.ObjectError;
@@ -24,12 +26,14 @@ import static java.util.stream.Collectors.toList;
 /**
  * @author aaristides
  */
+@Order(Ordered.HIGHEST_PRECEDENCE)
 @RestControllerAdvice
 @RequiredArgsConstructor
 public class ApiExceptionHandler {
 
-    private static final String NO_MESSSAGE_AVAILABLE = "No message available";
     private static final Logger LOG = LoggerFactory.getLogger(ApiExceptionHandler.class);
+
+    private static final String NO_MESSSAGE_AVAILABLE = "No message available";
 
     private final MessageSource apiErrorMessageSource;
 
@@ -56,16 +60,7 @@ public class ApiExceptionHandler {
         return ResponseEntity.badRequest().body(errorResponse);
     }
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorResponse> handlerException(Exception exception, Locale locale) {
-        LOG.error("Error not expected", exception);
-        final String errorCode = "error-1";
-        final HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
-        final ErrorResponse errorResponse = ErrorResponse.of(status, toApiError(errorCode, locale));
-        return ResponseEntity.status(status).body(errorResponse);
-    }
-
-    private ApiError toApiError(String code, Locale locale, Object... args) {
+    public ApiError toApiError(String code, Locale locale, Object... args) {
         String message;
         try {
             message = apiErrorMessageSource.getMessage(code, args, locale);
